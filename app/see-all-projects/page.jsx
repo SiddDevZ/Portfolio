a91a1bd81bb2460c3c73bc8580b60ef735dc57f4
SiddDevZ <epic.siddarth@gmail.com>
2025-07-20 08:34:24 +0530

    Scroll to top

--- app/see-all-projects/page.jsx
+++ app/see-all-projects/page.jsx
@@ -1,8 +1,12 @@
 "use client";
-import React from 'react'
+import React, { useEffect } from 'react'
 import Link from 'next/link'
 
 const Page = () => {
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
   const allProjects = [
     {
       title: "Lumeroo - Streaming Platform",
